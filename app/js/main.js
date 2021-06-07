8e15d9fa9d3e6aa6180e6c635a626a1f1eddc3b6
sava911 <pesss929@gmail.com>
2021-06-07 18:38:25 +0300

    start product block

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
-$(function(){
+$(function () {
 
+    $(".banner-section__slider").slick({
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        fade: true,
+        prevArrow: '<button><img class="banner-arrow banner-arrow__prev" src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/arrow-right.svg" alt=""></button>'
+    })
 
-  
+    $(".tab").on("click", function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass("tab--active");
+        $($(this).parent().siblings().find("div")).removeClass(
+            "tabs-contant--active"
+        );
+
+        $(this).addClass("tab--active");
+        $($(this).attr("href")).addClass("tabs-contant--active");
+    });
+
 });
